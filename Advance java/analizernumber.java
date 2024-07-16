71204b5b09bac6fd67a8fc728cf724973a77b9c5
code with sharma ji <sharma500cr@gmail.com>
2024-07-16 14:18:17 +0530

    analizernumber

---
+++ Advance java/analizernumber.java
@@ -0,0 +1,23 @@
+public class analizernumber {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.println("Enter the number of items");
+        int n = input.nextInt();
+        double [] numbers = new double[n];
+        double sum=0;
+
+        System.out.println("enter the number");
+        for(int i=0; i<n; i++)
+        {
+            numbers[i]=input.nextDouble();
+            sum+=numbers[i];
+        }
+        double average = sum/n;
+        int count=0;
+        for(int i=0; i<n; i++)
+        if(numbers[i]>average)
+        count++;
+        System.out.println("average is"+ average);
+        System.out.println("number of elements above the average is"+count);
+    }    
+}
