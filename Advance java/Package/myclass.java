adc263455c0be4aca028801e1b6c6470e8cf5d52
code with sharma ji <sharma500cr@gmail.com>
2024-07-17 13:43:40 +0530

    package class

---
+++ Advance java/Package/myclass.java
@@ -0,0 +1,8 @@
+package Package;
+
+public class myclass {
+    public void test()
+    {
+        System.out.println("welcome to myclass");
+    }    
+}
---
+++ Advance java/Packagetest.java
@@ -0,0 +1,8 @@
+import Package.myclass;
+
+public class Packagetest {
+    public static void main(String[] args) {
+        myclass theclass = new myclass();
+        theclass.test();
+    }    
+}
